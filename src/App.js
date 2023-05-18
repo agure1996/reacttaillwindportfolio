8c091f98dbffa9d88c6d0fd37cd6e731019a9ca3
A <abbasgure@gmail.com>
2023-05-18 17:38:42 +0100

    Set up the tailwind css navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from "react";
+import Header from './components/Header'
 
 
 function App() {
   return (
-    <div className="bg-black text-white">
-      Test
+    <div className="bg-teal-50 w-full h-screen">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+    <div className='md:flex bg-white py-4'>
+        <div>
+
+            <span>
+            <ion-icon name="settings-outline"></ion-icon>
+            </span>
+            Navbar
+        </div>
+
+    </div>
+    
+    </div>
+  )
+}
+
+export default Navbar
