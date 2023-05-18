1044a05f286d81c8d35cdf1c46085a3033419b94
A <abbasgure@gmail.com>
2023-05-18 22:02:55 +0100

    navbar burger menu done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Links from './Links'
+import Button from './Button'
 
 const Navbar = () => {
-  return (
-    <div className='shadow-md w-full fixed top-0 left-0'>
-    <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-        <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800'>
 
-            <span className='text-3xl text-fuchsia-700 mr-1 pt-2 hover:text-orange-300 duration-500'>
-            <a href="/"><ion-icon name="logo-ionic"></ion-icon></a>
-            </span>
-            Navbar
+    let [open, setOpen] = useState(false);
+    return (
+        <div className='shadow-md w-full fixed top-0 left-0'>
+            <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+                <div className='font-bold text-2xl cursor-pointer
+                        flex items-center font-[Poppins] text-gray-800'>
+
+                    <span className='text-3xl text-fuchsia-700 mr-1 pt-2 hover:text-orange-300 duration-500'>
+                        <a href="/"><ion-icon name="logo-ionic"></ion-icon></a>
+                    </span>
+                    Navbar
+                </div>
+
+                <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+                    <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
+                </div>
+                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full 
+                        md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100' : 'top-[-490px] opacity-0 '} md:opacity-100opacity-0`}>
+                    {Links.map((link) => (  
+                        <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+                            <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+                        </li>
+                    ))}
+
+                    <Button >
+                        Sign Up
+                    </Button>
+
+                </ul>
+            </div>
+
         </div>
-        <ul className='md:flex md:items-center'>
-           {Links.map((link)=> (
-            <li key={link.name} className='md:ml-8 text-xl'>
-                <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
-            </li>
-           ))}
-        </ul>
-    </div>
-    
-    </div>
-  )
+    )
 }
 
 export default Navbar
